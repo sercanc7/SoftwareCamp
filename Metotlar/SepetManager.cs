85f4b045535f3f1759e3fce4db6f159e56b450cc
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-11-11 23:34:33 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class SepetManager
+    {
+        // naming convention 
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urun.Adi);
+        }
+
+        // Yanlış yol !! ------> class olmadan böyle ekleme yapılmamalı.
+        public void Ekle2(string urunAdi , string aciklama, double fiyat,int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urunAdi);
+
+        }
+    }
+}
