0762ef70002dbc06811bddf894ef8d3b4e22db87
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-11-17 03:11:10 +0300

    Day 4 Homework

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyDictionary<Person,Number,Id>
+    {
+        Person[] _persons;
+        Number[] _numbers;
+        Id[] _ids;
+
+        public MyDictionary()
+        {
+            _persons = new Person[0];
+            _numbers = new Number[0];
+            _ids = new Id[0];
+        }
+
+        public void Add(Person person, Number number, Id ids)
+        {
+            Person[] tempPerson = _persons;
+            Number[] tempNumber = _numbers;
+            Id[] tempIds = _ids;
+
+            _persons = new Person[_persons.Length+1];
+            _numbers = new Number[_numbers.Length+1];
+            _ids = new Id[_ids.Length+1];
+
+
+            for (int i = 0; i < tempPerson.Length; i++)
+            {
+                _persons[i] = tempPerson[i];
+                _numbers[i] = tempNumber[i];
+                _ids[i] = tempIds[i];
+            }
+
+            bool containsKey = _numbers.Contains(number);
+            
+            if(containsKey)
+            {
+                string msg = (number + "nolu key eklenemez. Zaten tanımlı!");
+                Exception ex = new Exception(msg);
+                throw ex;
+            }
+
+            else
+            {
+                _numbers[_numbers.Length-1] = number;
+                _persons[_numbers.Length-1] = person;
+                _ids[_ids.Length-1] = ids;
+            }
+
+            Console.WriteLine("{0},{1},{2}",person,number,ids);
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,int,int>Person = new MyDictionary<string,int,int>();
+
+            Person.Add("Sercan", 1, 2);
+            Person.Add("Koray", 3, 4);
+            Person.Add("Neşe", 5, 6);
+            Person.Add("Selçuk", 7, 8); ;
+
+
+        }
+    }
+}
